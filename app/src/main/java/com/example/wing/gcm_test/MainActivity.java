0415e6de5bcdbfdb81a08e8d3dbbe87b4e3ace25
wingfchow <wayne54347@gmail.com>
2016-04-27 15:45:18 -0700

    Project initialized

---
+++ app/src/main/java/com/example/wing/gcm_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wing.gcm_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
